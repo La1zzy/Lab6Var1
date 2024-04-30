eae59a138821576ea692ef233d7736ca3b1077a3
La1zzy <rikonn1231@gmail.com>
2024-05-01 00:32:03 +0300

    Добавьте файлы проекта.

---
+++ Lab6Var1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6Var1
+{
+    partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            string textToPrint = richTextBox1.Text;
+            Font printFont = richTextBox1.Font;
+            Brush printBrush = new SolidBrush(richTextBox1.ForeColor);
+            e.Graphics.DrawString(textToPrint, printFont, printBrush, 10, 10);
+        }
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                richTextBox1.Text = System.IO.File.ReadAllText(filePath);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                System.IO.File.WriteAllText(filePath, richTextBox1.Text);
+            }
+        }
+
+        private void viewFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
+            PrintDocument printDocument1 = new PrintDocument();
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            printPreviewDialog.Document = printDocument1;
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument1 = new PrintDocument();
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            PrintDialog printDialog1 = new PrintDialog();
+            printDialog1.Document = printDocument1;
+
+            DialogResult result = printDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+        }
+
+        private void fontChangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog.Font;
+            }
+        }
+
+        private void changecolorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog.Color;
+            }
+        }
+
+        private void aboutProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The program was created for Lab6");
+        }
+    }
+}
